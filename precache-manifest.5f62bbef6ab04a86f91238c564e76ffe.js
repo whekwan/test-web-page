15e5e3fdc05a8ce1fd80edcf5b788294aba34c51
whekwan <whekwan@connect.ust.hk>
2019-09-08 23:26:32 +0800

    Updates

---
+++ precache-manifest.5f62bbef6ab04a86f91238c564e76ffe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42253b8a5cab93fcff80a82192e7d1c2",
+    "url": "/test-web-page/index.html"
+  },
+  {
+    "revision": "54ee1c936b4cbbcc01cb",
+    "url": "/test-web-page/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "248fd5a1d789a50dcc52",
+    "url": "/test-web-page/static/js/2.0ada5135.chunk.js"
+  },
+  {
+    "revision": "54ee1c936b4cbbcc01cb",
+    "url": "/test-web-page/static/js/main.ef7da92e.chunk.js"
+  },
+  {
+    "revision": "a0b74a643365b3c416aa",
+    "url": "/test-web-page/static/js/runtime~main.bbc01dee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-web-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
